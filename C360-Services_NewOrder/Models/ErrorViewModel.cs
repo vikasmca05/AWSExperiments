150a64362ff0930ab57aefdfb215f0b618b0f20c
vikasmca05 <vikasmca05@gmail.com>
2020-03-24 22:14:42 -0700

    Initial check-in of module C360-Services_NewOrder

---
+++ C360-Services_NewOrder/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace C360_Services_NewOrder.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
